063705f24dc63585e0c56dbbc1345c2318da8207
Arshjot Dua <109437882+Arsh7707@users.noreply.github.com>
2024-09-12 09:02:25 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Sidenav from './components/Sidenav'
+import Main from './components/Main'
+import Project from './components/Project'
+import Contact from './components/Contact'
+import Skills from './components/Skills'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+      <div>
+        <Sidenav></Sidenav>
+        <Main></Main>
+        <Project></Project>
+        <Contact></Contact>
+        <Skills></Skills>
+       </div>
+  )
+}
+
+export default App
